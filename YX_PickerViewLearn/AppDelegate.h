2fd44adedb61eac963fadcdb039d97b725ecbaff
populary <410445919@qq.com>
2016-08-29 20:38:17 +0800

    Initial Commit

---
+++ YX_PickerViewLearn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YX_PickerViewLearn
+//
+//  Created by yang on 16/8/29.
+//  Copyright © 2016年 poplary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YX_PickerViewLearn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YX_PickerViewLearn
+//
+//  Created by yang on 16/8/29.
+//  Copyright © 2016年 poplary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
